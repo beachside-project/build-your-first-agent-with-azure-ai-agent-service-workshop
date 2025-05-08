bb1d3a55b01082cd8b1127b09a23c734c1725292
Atsushi YOKOHAMA <yokohama@zengeeks.io>
2025-05-09 05:27:39 +0900

    Fix command

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -43,7 +43,7 @@ if [ -f output.json ]; then
     HOST_NAME=$(echo "$DISCOVERY_URL" | sed -e 's|^https://||' -e 's|/discovery$||')
 
     # Generate the PROJECT_CONNECTION_STRING
-    PROJECT_CONNECTION_STRING="\"$HOST_NAME;$SUBSCRIPTION_ID;$RESOURCE_GROUP_NAME;$AI_PROJECT_NAME\""
+    PROJECT_CONNECTION_STRING="$HOST_NAME;$SUBSCRIPTION_ID;$RESOURCE_GROUP_NAME;$AI_PROJECT_NAME"
 
     ENV_FILE_PATH="../src/python/workshop/.env"
 
